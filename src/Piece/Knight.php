29678321ce7848846ed6d0764b79e49c809c9673
Jordi <info@programarivm.com>
2017-06-22 21:00:23 +0100

    Testing Knight...

--- src/Piece/Knight.php
+++ src/Piece/Knight.php
@@ -8,10 +8,103 @@ class Knight extends AbstractPiece
 {
     public function __construct($color, $position)
     {
-        parent::__construct($color, $position, PGN::PIECE_KNIGHT);
+        parent::__construct($color, $position, PGN::PIECE_BISHOP);
+        $this->position->scope = (object)[
+            'jumps' => []
+        ];
+        $this->scope();
     }
 
     protected function scope()
     {
+        try
+        {
+            $file = chr(ord($this->position->current[0]) - 1);
+            $rank = (int)$this->position->current[1] + 2;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+            $file = chr(ord($this->position->current[0]) - 2);
+            $rank = (int)$this->position->current[1] + 1;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+            $file = chr(ord($this->position->current[0]) - 2);
+            $rank = (int)$this->position->current[1] - 1;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+            $file = chr(ord($this->position->current[0]) - 1);
+            $rank = (int)$this->position->current[1] - 2;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+            $file = chr(ord($this->position->current[0]) + 1);
+            $rank = (int)$this->position->current[1] - 2;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+
+            $file = chr(ord($this->position->current[0]) + 2);
+            $rank = (int)$this->position->current[1] - 1;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+            $file = chr(ord($this->position->current[0]) + 2);
+            $rank = (int)$this->position->current[1] + 1;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+
+        try
+        {
+            $file = chr(ord($this->position->current[0]) + 1);
+            $rank = (int)$this->position->current[1] + 2;
+            if(PGN::square($file.$rank, true))
+            {
+                $this->position->scope->jumps[] = $file . $rank;
+            }
+        }
+        catch (\InvalidArgumentException $e) {}
+            
     }
 }
--- tests/Piece/KnightTest.php
+++ tests/Piece/KnightTest.php
@@ -6,9 +6,40 @@ use PGNChess\Piece\Knight;
 
 class KnightTest extends \PHPUnit_Framework_TestCase
 {
-  public function testInstantiate()
-  {
-    $knight = new Knight(PGN::COLOR_WHITE, 'b1');
-    $this->assertInstanceOf(Knight::class, $knight);
-  }
+    public function testScopeD4()
+    {
+        $knight = new Knight(PGN::COLOR_WHITE, 'd4');
+        $example = [
+            'c6',
+            'b5',
+            'b3',
+            'c2',
+            'e2',
+            'f3',
+            'f5',
+            'e6'
+        ];
+        $this->assertEquals($example, $knight->getPosition()->scope->jumps);
+    }
+
+    public function testScopeH1()
+    {
+        $knight = new Knight(PGN::COLOR_WHITE, 'h1');
+        $example = [
+            'g3',
+            'f2'
+        ];
+        $this->assertEquals($example, $knight->getPosition()->scope->jumps);
+    }
+
+    public function testScopeB1()
+    {
+        $knight = new Knight(PGN::COLOR_WHITE, 'b1');
+        $example = [
+            'a3',
+            'd2',
+            'c3'
+        ];
+        $this->assertEquals($example, $knight->getPosition()->scope->jumps);
+    }
 }

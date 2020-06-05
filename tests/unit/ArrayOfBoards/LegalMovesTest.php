0a2514d176093316333685c67d63388931a671e2
programarivm <info@programarivm.com>
2020-06-05 19:57:14 +0100

    sicilian defense passes the tests on multigame mode (four boards)

--- tests/unit/ArrayOfBoards/LegalMovesTest.php
+++ tests/unit/ArrayOfBoards/LegalMovesTest.php
@@ -9,29 +9,57 @@ use PGNChess\Tests\AbstractUnitTestCase;
 
 class LegalMovesTest extends AbstractUnitTestCase
 {
-    protected static $boards = [];
-
-    protected function setUp()
+    /**
+     * @test
+     */
+    public function e4_e5_d4_exd4_on_two_boards()
     {
-        self::$boards[0] = new Board();
-        self::$boards[1] = new Board();
+        $boards = [];
+        $boards[] = new Board();
+        $boards[] = new Board();
+
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::WHITE, 'e4')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::WHITE, 'e4')));
+
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::BLACK, 'e5')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::BLACK, 'e5')));
+
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::WHITE, 'd4')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::WHITE, 'd4')));
+
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::BLACK, 'exd4')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::BLACK, 'exd4')));
     }
 
     /**
      * @test
      */
-    public function e4_e5()
+    public function e4_c5_Nf3_d6_on_four_boards()
     {
-        $this->assertEquals(true, self::$boards[0]->play(Convert::toObject(Symbol::WHITE, 'e4')));
-        $this->assertEquals(true, self::$boards[1]->play(Convert::toObject(Symbol::WHITE, 'e4')));
+        $boards = [];
+        $boards[] = new Board();
+        $boards[] = new Board();
+        $boards[] = new Board();
+        $boards[] = new Board();
 
-        $this->assertEquals(true, self::$boards[0]->play(Convert::toObject(Symbol::BLACK, 'e5')));
-        $this->assertEquals(true, self::$boards[1]->play(Convert::toObject(Symbol::BLACK, 'e5')));
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::WHITE, 'e4')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::WHITE, 'e4')));
+        $this->assertEquals(true, $boards[2]->play(Convert::toObject(Symbol::WHITE, 'e4')));
+        $this->assertEquals(true, $boards[3]->play(Convert::toObject(Symbol::WHITE, 'e4')));
 
-        $this->assertEquals(true, self::$boards[0]->play(Convert::toObject(Symbol::WHITE, 'd4')));
-        $this->assertEquals(true, self::$boards[1]->play(Convert::toObject(Symbol::WHITE, 'd4')));
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::BLACK, 'c5')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::BLACK, 'c5')));
+        $this->assertEquals(true, $boards[2]->play(Convert::toObject(Symbol::BLACK, 'c5')));
+        $this->assertEquals(true, $boards[3]->play(Convert::toObject(Symbol::BLACK, 'c5')));
 
-        $this->assertEquals(true, self::$boards[0]->play(Convert::toObject(Symbol::BLACK, 'exd4')));
-        $this->assertEquals(true, self::$boards[1]->play(Convert::toObject(Symbol::BLACK, 'exd4')));
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::WHITE, 'Nf3')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::WHITE, 'Nf3')));
+        $this->assertEquals(true, $boards[2]->play(Convert::toObject(Symbol::WHITE, 'Nf3')));
+        $this->assertEquals(true, $boards[3]->play(Convert::toObject(Symbol::WHITE, 'Nf3')));
+
+        $this->assertEquals(true, $boards[0]->play(Convert::toObject(Symbol::BLACK, 'd6')));
+        $this->assertEquals(true, $boards[1]->play(Convert::toObject(Symbol::BLACK, 'd6')));
+        $this->assertEquals(true, $boards[2]->play(Convert::toObject(Symbol::BLACK, 'd6')));
+        $this->assertEquals(true, $boards[3]->play(Convert::toObject(Symbol::BLACK, 'd6')));
     }
 }

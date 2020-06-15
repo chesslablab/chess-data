d3e631cc0d94a49888e95af59c8a69675192a3f2
programarivm <info@programarivm.com>
2020-06-15 22:25:36 +0100

    pgn conversion cleanup

--- src/PGN/Convert.php
+++ src/PGN/Convert.php
@@ -46,7 +46,6 @@ class Convert
                         'current' => null,
                         'next' => mb_substr($pgn, -2)
                 ]];
-                break;
 
             case preg_match('/^' . Move::KING_CASTLING_SHORT . '$/', $pgn):
                 return (object) [
@@ -58,7 +57,6 @@ class Convert
                     'identity' => Symbol::KING,
                     'position' => Castling::info($color)->{Symbol::KING}->{Symbol::CASTLING_SHORT}->position
                 ];
-                break;
 
             case preg_match('/^' . Move::KING_CASTLING_LONG . '$/', $pgn):
                 return (object) [
@@ -70,7 +68,6 @@ class Convert
                     'identity' => Symbol::KING,
                     'position' => Castling::info($color)->{Symbol::KING}->{Symbol::CASTLING_LONG}->position
                 ];
-                break;
 
             case preg_match('/^' . Move::KING_CAPTURES . '$/', $pgn):
                 return (object) [
@@ -84,7 +81,6 @@ class Convert
                         'current' => null,
                         'next' => mb_substr($pgn, -2)
                 ]];
-                break;
 
             case preg_match('/^' . Move::PIECE . '$/', $pgn):
                 if (!$isCheck) {
@@ -105,7 +101,6 @@ class Convert
                         'current' => $currentPosition,
                         'next' => $nextPosition
                 ]];
-                break;
 
             case preg_match('/^' . Move::PIECE_CAPTURES . '$/', $pgn):
                 return (object) [
@@ -119,7 +114,6 @@ class Convert
                         'current' => !$isCheck ? mb_substr(mb_substr($pgn, 0, -3), 1) : mb_substr(mb_substr($pgn, 0, -4), 1),
                         'next' => !$isCheck ? mb_substr($pgn, -2) : mb_substr($pgn, -3, -1)
                 ]];
-                break;
 
             case preg_match('/^' . Move::KNIGHT . '$/', $pgn):
                 if (!$isCheck) {
@@ -140,7 +134,6 @@ class Convert
                         'current' => $currentPosition,
                         'next' => $nextPosition
                 ]];
-                break;
 
             case preg_match('/^' . Move::KNIGHT_CAPTURES . '$/', $pgn):
                 return (object) [
@@ -154,7 +147,6 @@ class Convert
                         'current' => !$isCheck ? mb_substr(mb_substr($pgn, 0, -3), 1) : mb_substr(mb_substr($pgn, 0, -4), 1),
                         'next' => !$isCheck ? mb_substr($pgn, -2) : mb_substr($pgn, -3, -1)
                 ]];
-                break;
 
             case preg_match('/^' . Move::PAWN . '$/', $pgn):
                 return (object) [
@@ -168,7 +160,6 @@ class Convert
                         'current' => mb_substr($pgn, 0, 1),
                         'next' => !$isCheck ? $pgn : mb_substr($pgn, 0, -1)
                 ]];
-                break;
 
             case preg_match('/^' . Move::PAWN_CAPTURES . '$/', $pgn):
                 return (object) [
@@ -182,7 +173,6 @@ class Convert
                         'current' => mb_substr($pgn, 0, 1),
                         'next' => !$isCheck ? mb_substr($pgn, -2) : mb_substr($pgn, -3, -1)
                 ]];
-                break;
 
             case preg_match('/^' . Move::PAWN_PROMOTES . '$/', $pgn):
                 return (object) [
@@ -197,7 +187,6 @@ class Convert
                         'current' => null,
                         'next' => mb_substr($pgn, 0, 2)
                 ]];
-                break;
 
             case preg_match('/^' . Move::PAWN_CAPTURES_AND_PROMOTES . '$/', $pgn):
                 return (object) [
@@ -212,11 +201,9 @@ class Convert
                         'current' => null,
                         'next' => mb_substr($pgn, 2, 2)
                 ]];
-                break;
 
             default:
                 throw new UnknownNotationException("Unknown PGN notation.");
-                break;
         }
     }
 
@@ -231,22 +218,16 @@ class Convert
         switch($identity) {
             case Symbol::BISHOP:
                 return (new \ReflectionClass('\PGNChess\Piece\Bishop'))->getName();
-                break;
             case Symbol::KING:
                 return (new \ReflectionClass('\PGNChess\Piece\King'))->getName();
-                break;
             case Symbol::KNIGHT:
                 return (new \ReflectionClass('\PGNChess\Piece\Knight'))->getName();
-                break;
             case Symbol::PAWN:
                 return (new \ReflectionClass('\PGNChess\Piece\Pawn'))->getName();
-                break;
             case Symbol::QUEEN:
                 return (new \ReflectionClass('\PGNChess\Piece\Queen'))->getName();
-                break;
             case Symbol::ROOK:
                 return (new \ReflectionClass('\PGNChess\Piece\Rook'))->getName();
-                break;
         }
     }
 }

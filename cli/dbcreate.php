625f7156b6b0f308f532ecd78982a2ef1acbdb3b
programarivm <info@programarivm.com>
2020-06-21 12:27:50 +0100

    optimizing MySQL data types

--- cli/dbcreate.php
+++ cli/dbcreate.php
@@ -31,49 +31,49 @@ $sql = 'DROP TABLE IF EXISTS games';
 Pdo::getInstance()->query($sql);
 
 $sql = 'CREATE TABLE games (' .
-    Tag::EVENT                      . ' VARCHAR(64) NULL, ' . // STR (Seven Tag Roster)
-    Tag::SITE                       . ' VARCHAR(64) NULL, ' .
-    Tag::DATE                       . ' VARCHAR(64) NULL, ' .
-    Tag::ROUND                      . ' VARCHAR(8) NULL, ' .
-    Tag::WHITE                      . ' VARCHAR(64) NULL, ' .
-    Tag::BLACK                      . ' VARCHAR(64) NULL, ' .
-    Tag::RESULT                     . ' VARCHAR(8) NULL, ' .
-    Tag::FICS_GAMES_DB_GAME_NO      . ' VARCHAR(32) NULL, ' . // FICS database
-    Tag::WHITE_TITLE                . ' VARCHAR(16) NULL, ' . // player related information
-    Tag::BLACK_TITLE                . ' VARCHAR(16) NULL, ' .
-    Tag::WHITE_ELO                  . ' VARCHAR(8) NULL, ' .
-    Tag::BLACK_ELO                  . ' VARCHAR(8) NULL, ' .
-    Tag::WHITE_USCF                 . ' VARCHAR(8) NULL, ' .
-    Tag::BLACK_USCF                 . ' VARCHAR(8) NULL, ' .
-    Tag::WHITE_NA                   . ' VARCHAR(8) NULL, ' .
-    Tag::BLACK_NA                   . ' VARCHAR(8) NULL, ' .
-    Tag::WHITE_TYPE                 . ' VARCHAR(8) NULL, ' .
-    Tag::BLACK_TYPE                 . ' VARCHAR(8) NULL, ' .
-    Tag::EVENT_DATE                 . ' VARCHAR(32) NULL, ' . // event related information
-    Tag::EVENT_SPONSOR              . ' VARCHAR(64) NULL, ' .
-    Tag::SECTION                    . ' VARCHAR(32) NULL, ' .
-    Tag::STAGE                      . ' VARCHAR(32) NULL, ' .
-    Tag::BOARD                      . ' VARCHAR(32) NULL, ' .
-    Tag::OPENING                    . ' VARCHAR(32) NULL, ' . // opening information
-    Tag::VARIATION                  . ' VARCHAR(32) NULL, ' .
-    Tag::SUB_VARIATION              . ' VARCHAR(32) NULL, ' .
-    Tag::ECO                        . ' VARCHAR(32) NULL, ' .
-    Tag::NIC                        . ' VARCHAR(32) NULL, ' .
-    Tag::TIME                       . ' VARCHAR(32) NULL, ' . // time and date related information
-    Tag::TIME_CONTROL               . ' VARCHAR(32) NULL, ' .
-    Tag::UTC_TIME                   . ' VARCHAR(32) NULL, ' .
-    Tag::UTC_DATE                   . ' VARCHAR(32) NULL, ' .
-    Tag::WHITE_CLOCK                . ' VARCHAR(32) NULL, ' . // clock
-    Tag::BLACK_CLOCK                . ' VARCHAR(32) NULL, ' .
-    Tag::SET_UP                     . ' VARCHAR(32) NULL, ' . // alternative starting positions
-    Tag::FEN                        . ' VARCHAR(32) NULL, ' .
-    Tag::TERMINATION                . ' VARCHAR(32) NULL, ' . // game conclusion
-    Tag::ANNOTATOR                  . ' VARCHAR(32) NULL, ' . // miscellaneous
-    Tag::MODE                       . ' VARCHAR(32) NULL, ' .
-    Tag::PLY_COUNT                  . ' VARCHAR(32) NULL, ' .
-    Tag::WHITE_RD                   . ' VARCHAR(32) NULL, ' .
-    Tag::BLACK_RD                   . ' VARCHAR(32) NULL, ' .
-    'movetext  TEXT NOT NULL
-)';
+    Tag::EVENT                      . ' CHAR(64) NULL, ' . // STR (Seven Tag Roster)
+    Tag::SITE                       . ' CHAR(64) NULL, ' .
+    Tag::DATE                       . ' CHAR(16) NULL, ' .
+    Tag::ROUND                      . ' CHAR(8) NULL, ' .
+    Tag::WHITE                      . ' CHAR(32) NULL, ' .
+    Tag::BLACK                      . ' CHAR(32) NULL, ' .
+    Tag::RESULT                     . ' CHAR(8) NULL, ' .
+    Tag::FICS_GAMES_DB_GAME_NO      . ' CHAR(16) NULL, ' . // FICS database
+    Tag::WHITE_TITLE                . ' CHAR(16) NULL, ' . // player related information
+    Tag::BLACK_TITLE                . ' CHAR(16) NULL, ' .
+    Tag::WHITE_ELO                  . ' CHAR(8) NULL, ' .
+    Tag::BLACK_ELO                  . ' CHAR(8) NULL, ' .
+    Tag::WHITE_USCF                 . ' CHAR(8) NULL, ' .
+    Tag::BLACK_USCF                 . ' CHAR(8) NULL, ' .
+    Tag::WHITE_NA                   . ' CHAR(8) NULL, ' .
+    Tag::BLACK_NA                   . ' CHAR(8) NULL, ' .
+    Tag::WHITE_TYPE                 . ' CHAR(16) NULL, ' .
+    Tag::BLACK_TYPE                 . ' CHAR(16) NULL, ' .
+    Tag::EVENT_DATE                 . ' CHAR(16) NULL, ' . // event related information
+    Tag::EVENT_SPONSOR              . ' CHAR(32) NULL, ' .
+    Tag::SECTION                    . ' CHAR(16) NULL, ' .
+    Tag::STAGE                      . ' CHAR(32) NULL, ' .
+    Tag::BOARD                      . ' CHAR(8) NULL, ' .
+    Tag::OPENING                    . ' CHAR(32) NULL, ' . // opening information
+    Tag::VARIATION                  . ' CHAR(32) NULL, ' .
+    Tag::SUB_VARIATION              . ' CHAR(32) NULL, ' .
+    Tag::ECO                        . ' CHAR(32) NULL, ' .
+    Tag::NIC                        . ' CHAR(32) NULL, ' .
+    Tag::TIME                       . ' CHAR(16) NULL, ' . // time and date related information
+    Tag::TIME_CONTROL               . ' CHAR(16) NULL, ' .
+    Tag::UTC_TIME                   . ' CHAR(16) NULL, ' .
+    Tag::UTC_DATE                   . ' CHAR(16) NULL, ' .
+    Tag::WHITE_CLOCK                . ' CHAR(16) NULL, ' . // clock
+    Tag::BLACK_CLOCK                . ' CHAR(16) NULL, ' .
+    Tag::SET_UP                     . ' CHAR(8) NULL, ' . // alternative starting positions
+    Tag::FEN                        . ' CHAR(64) NULL, ' .
+    Tag::TERMINATION                . ' CHAR(32) NULL, ' . // game conclusion
+    Tag::ANNOTATOR                  . ' CHAR(32) NULL, ' . // miscellaneous
+    Tag::MODE                       . ' CHAR(16) NULL, ' .
+    Tag::PLY_COUNT                  . ' CHAR(4) NULL, ' .
+    Tag::WHITE_RD                   . ' CHAR(8) NULL, ' .
+    Tag::BLACK_RD                   . ' CHAR(8) NULL, ' .
+    'movetext  VARCHAR(3072)
+) ENGINE = MYISAM';
 
 Pdo::getInstance()->query($sql);

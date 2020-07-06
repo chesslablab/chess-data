8a30c6c154fcfcc583b61fcfbc944e33aac0a6bf
programarivm <info@programarivm.com>
2020-07-06 19:40:20 +0100

    the results of a few evaluation functions are loaded into the database

--- src/File/Seed.php
+++ src/File/Seed.php
@@ -3,6 +3,10 @@
 namespace PGNChessData\File;
 
 use PGNChess\Exception\UnknownNotationException;
+use PGNChess\Heuristic\AttackSnapshot;
+use PGNChess\Heuristic\CenterSnapshot;
+use PGNChess\Heuristic\MaterialSnapshot;
+use PGNChess\Heuristic\SpaceSnapshot;
 use PGNChess\PGN\Tag;
 use PGNChess\PGN\Validate;
 use PGNChessData\Pdo;
@@ -68,13 +72,35 @@ class Seed extends AbstractFile
             $sql .= "$name, ";
         }
 
-        $sql .= "movetext) VALUES ($params:movetext)";
+        $sql .= "movetext, attack, center, material, space) VALUES ($params:movetext, :attack, :center, :material, :space)";
 
-        $values[] = [
-            'param' => ':movetext',
-            'value' => $movetext,
-            'type' => \PDO::PARAM_STR
-        ];
+        array_push($values,
+            [
+                'param' => ':movetext',
+                'value' => $movetext,
+                'type' => \PDO::PARAM_STR
+            ],
+            [
+                'param' => ':attack',
+                'value' => json_encode((new AttackSnapshot($movetext))->take()),
+                'type' => \PDO::PARAM_STR
+            ],
+            [
+                'param' => ':center',
+                'value' => json_encode((new CenterSnapshot($movetext))->take()),
+                'type' => \PDO::PARAM_STR
+            ],
+            [
+                'param' => ':material',
+                'value' => json_encode((new MaterialSnapshot($movetext))->take()),
+                'type' => \PDO::PARAM_STR
+            ],
+            [
+                'param' => ':space',
+                'value' => json_encode((new SpaceSnapshot($movetext))->take()),
+                'type' => \PDO::PARAM_STR
+            ]
+        );
 
         try {
             return Pdo::getInstance()->query($sql, $values);

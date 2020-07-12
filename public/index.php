8505b5193b568cf13ab6e1ca882d535c87ff8127
programarivm <info@programarivm.com>
2020-07-12 15:33:28 +0100

    wide-open CORS is set up

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
 <?php
 
-define('APP_PATH', dirname(dirname(__FILE__)));
+require realpath(dirname(__FILE__)) .'/../src/bootstrap.php';
 
-require APP_PATH.'/vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(APP_PATH);
-$dotenv->load();
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range");
 
 switch (true) {
     case '/api/query' === $_SERVER['REQUEST_URI'] && $_SERVER['REQUEST_METHOD'] === 'POST':
         require APP_PATH . '/src/Api/Query.php';
+        exit;
+    case $_SERVER['REQUEST_METHOD'] === 'OPTIONS':
+        http_response_code(204);
         exit;
     default:
         http_response_code(404);

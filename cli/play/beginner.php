d7c442fc0d5c82ead82f599d9003a4aa0762d1ad
programarivm <info@programarivm.com>
2020-08-09 17:41:35 +0100

    dimensionality fix in cli/train/beginner.php

--- cli/play/beginner.php
+++ cli/play/beginner.php
@@ -2,62 +2,10 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use PGNChess\Board;
-use PGNChess\ML\Supervised\Regression\Labeller\Primes\Decoder as PrimesLabelDecoder;
-use PGNChess\ML\Supervised\Regression\Sampler\Primes\Sampler as PrimesSampler;
-use PGNChess\PGN\Convert;
-use PGNChess\PGN\Symbol;
-use Rubix\ML\PersistentModel;
-use Rubix\ML\Persisters\Filesystem;
+use PGNChess\Game;
 
-const DATA_FOLDER = __DIR__.'/../../model';
+$game = new Game(Game::MODE_PVA);
+$game->play('w', 'e4');
 
-$estimator = PersistentModel::load(new Filesystem(DATA_FOLDER.'/beginner.model'));
-
-$board = new Board;
-
-
-$board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
-echo "w e4" . PHP_EOL;
-
-$sample = (new PrimesSampler($board))->sample();
-$prediction = $estimator->predictSample($sample[Symbol::WHITE]);
-$decoded = (new PrimesLabelDecoder($board))->decode(Symbol::BLACK, $prediction);
-
-// b6
-echo "b {$decoded}" . PHP_EOL;
-$board->play(Convert::toStdObj(Symbol::BLACK, $decoded));
-$board->play(Convert::toStdObj(Symbol::WHITE, 'd4'));
-echo "w d4" . PHP_EOL;
-
-$sample = (new PrimesSampler($board))->sample();
-$prediction = $estimator->predictSample($sample[Symbol::WHITE]);
-$decoded = (new PrimesLabelDecoder($board))->decode(Symbol::BLACK, $prediction);
-
-// b5
-echo "b {$decoded}" . PHP_EOL;
-$board->play(Convert::toStdObj(Symbol::BLACK, $decoded));
-$board->play(Convert::toStdObj(Symbol::WHITE, 'Nf3'));
-echo "w Nf3" . PHP_EOL;
-
-$sample = (new PrimesSampler($board))->sample();
-$prediction = $estimator->predictSample($sample[Symbol::WHITE]);
-$decoded = (new PrimesLabelDecoder($board))->decode(Symbol::BLACK, $prediction);
-
-// Nc6
-echo "b {$decoded}" . PHP_EOL;
-$board->play(Convert::toStdObj(Symbol::BLACK, $decoded));
-$board->play(Convert::toStdObj(Symbol::WHITE, 'Bxb5'));
-echo "w Bxb5" . PHP_EOL;
-
-$sample = (new PrimesSampler($board))->sample();
-$prediction = $estimator->predictSample($sample[Symbol::WHITE]);
-$decoded = (new PrimesLabelDecoder($board))->decode(Symbol::BLACK, $prediction);
-
-// Nh6
-echo "b {$decoded}" . PHP_EOL;
-$board->play(Convert::toStdObj(Symbol::BLACK, $decoded));
-$board->play(Convert::toStdObj(Symbol::WHITE, 'Bxc6'));
-echo "w Bxc6" . PHP_EOL;
-
-echo $board->getMovetext() . PHP_EOL;
+// TODO 
+echo $game->response();

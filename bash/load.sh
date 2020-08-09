a47a2a97a9e1bebe70f5e305f396c5d625072006
programarivm <info@programarivm.com>
2020-08-09 16:20:06 +0100

    a few thousand games of chess grandmasters have been prepared

--- bash/load.sh
+++ bash/load.sh
@@ -14,7 +14,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 APP_PATH="$(dirname $DIR)"
 cd $APP_PATH
 
-for file in data/games/*
+for file in data/players/*
 do
   php cli/db/seed.php $file --quiet $1
   echo "Loading games for $SECONDS s...";

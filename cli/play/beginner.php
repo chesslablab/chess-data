cf7d5c30ab8ffdc7b07b78432605b4744f019a4c
programarivm <info@programarivm.com>
2020-08-11 23:18:49 +0100

    beginner.model is trained with 15,000 games using labeled samples with a dimensionality of 7

--- cli/play/beginner.php
+++ cli/play/beginner.php
@@ -7,19 +7,7 @@ use PGNChess\Game;
 $game = new Game(Game::MODE_PVA);
 $game->play('w', 'e4');
 $game->play('b', $game->response());
-$game->play('w', 'Nf3');
-$game->play('b', $game->response());
-$game->play('w', 'Bb5');
-$game->play('b', $game->response());
-$game->play('w', 'Bxd7');
-$game->play('b', $game->response());
-$game->play('w', 'Bxd7');
-$game->play('b', $game->response());
-$game->play('w', 'O-O');
-$game->play('b', $game->response());
-$game->play('w', 'd4');
-$game->play('b', $game->response());
-$game->play('w', 'cxd5');
+
 
 // TODO
 echo $game->movetext() . PHP_EOL;

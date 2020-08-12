73db02aa99f80bc0e64869fbe6ae8908a9fc6ff5
programarivm <info@programarivm.com>
2020-08-12 22:18:42 +0100

    beginner.model is trained with 28,000 games

--- cli/play/beginner.php
+++ cli/play/beginner.php
@@ -7,7 +7,16 @@ use PGNChess\Game;
 $game = new Game(Game::MODE_PVA);
 $game->play('w', 'e4');
 $game->play('b', $game->response());
-
+$game->play('w', 'Bc4');
+$game->play('b', $game->response());
+$game->play('w', 'Nf3');
+$game->play('b', $game->response());
+$game->play('w', 'O-O');
+$game->play('b', $game->response());
+$game->play('w', 'd4');
+$game->play('b', $game->response());
+$game->play('w', 'Bxb5');
+$game->play('b', $game->response());
 
 // TODO
 echo $game->movetext() . PHP_EOL;

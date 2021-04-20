278d31915720fef5ef3d45aff1475aa828ce22d6
programarivm <info@programarivm.com>
2021-04-20 11:57:52 +0200

    Updated beginner player after training

--- cli/play/beginner.php
+++ cli/play/beginner.php
@@ -7,14 +7,8 @@ use Chess\Game;
 $game = new Game(Game::MODE_PVA);
 $game->play('w', 'e4');
 $game->play('b', $game->response());
-$game->play('w', 'd4');
-$game->play('b', $game->response());
 $game->play('w', 'e5');
 $game->play('b', $game->response());
-$game->play('w', 'e6');
-$game->play('b', $game->response());
-
-
 
 // TODO
 echo $game->movetext() . PHP_EOL;

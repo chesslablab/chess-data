c3cff67cde39db323496e833317fdd7098d88f32
programarivm <info@programarivm.com>
2021-04-21 18:38:31 +0200

    Updated AbstractTestCase

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -9,10 +9,5 @@ class AbstractTestCase extends TestCase
     public function __construct($name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-
-        if ($_ENV['APP_ENV'] !== 'test') {
-            echo 'The tests can run on test environment only.' . PHP_EOL;
-            exit;
-        }
     }
 }

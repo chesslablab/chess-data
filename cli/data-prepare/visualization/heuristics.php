0e20f49cda016d6c649fc2913de3ea4e42456445
programarivm <info@programarivm.com>
2021-04-26 16:45:21 +0200

    Refactored the data preparation for visualization

--- cli/data-prepare/visualization/heuristics.php
+++ cli/data-prepare/visualization/heuristics.php
@@ -21,7 +21,7 @@ class DataPrepareCli extends CLI
 
         $options->setHelp('Creates a prepared dataset of heuristics in JSON format for further visualization.');
         $options->registerArgument('n', 'The number of games.', true);
-        $options->registerArgument('filename', 'The filename.', true);
+        $options->registerArgument('filename', 'The dataset filename.', true);
         $options->registerOption('win', 'White win.');
         $options->registerOption('lose', 'White lose.');
         $options->registerOption('draw', 'Draw.');
@@ -37,16 +37,25 @@ class DataPrepareCli extends CLI
             $result = '1/2-1/2';
         }
 
-        $sql = "SELECT * FROM games WHERE result='$result' ORDER BY RAND() LIMIT {$options->getArgs()[0]}";
+        $opt = key($options->getOpt());
+        $filename = "{$this->snakeCase($options->getArgs()[1])}_{$opt}.json";
+
+        $sql = "SELECT * FROM games WHERE White SOUNDS LIKE '{$options->getArgs()[1]}'
+            ORDER BY RAND()
+            LIMIT {$options->getArgs()[0]}";
 
         $games = Pdo::getInstance()
                     ->query($sql)
                     ->fetchAll(\PDO::FETCH_ASSOC);
 
-        $filename = $options->getArgs()[1];
         $fp = fopen(self::DATA_FOLDER."/$filename", 'w');
         fwrite($fp, json_encode($games));
         fclose($fp);
+    }
+
+    protected function snakeCase(string $string)
+    {
+        return str_replace(' ', '_', strtolower(trim($string)));
     }
 }
 

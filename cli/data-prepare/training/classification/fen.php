362868fc183879742df410a44c2067cbf6461e40
programarivm <info@programarivm.com>
2021-11-08 16:35:03 +0100

    Added script to prepare data from FEN positions

---
+++ cli/data-prepare/training/classification/fen.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace ChessData\Cli\DataPrepare\Training;
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use Chess\HeuristicPicture;
+use Chess\Combinatorics\RestrictedPermutationWithRepetition;
+use Chess\FEN\StringToBoard;
+use Chess\ML\Supervised\Classification\LinearCombinationLabeller;
+use Chess\PGN\Movetext;
+use Chess\PGN\Symbol;
+use ChessData\Pdo;
+use splitbrain\phpcli\CLI;
+use splitbrain\phpcli\Options;
+
+/**
+ * Prepares the data.
+ *
+ * It loads games from the database and plays them from a FEN position.
+ */
+class Fen extends CLI
+{
+    const DATA_FOLDER = __DIR__.'/../../../../dataset/training/classification';
+
+    protected function setup(Options $options)
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__.'/../../../../');
+        $dotenv->load();
+
+        $options->setHelp('Creates a prepared CSV dataset in the dataset/training/classification folder.');
+        $options->registerArgument('n', 'A random number of games to be queried.', true);
+    }
+
+    protected function main(Options $options)
+    {
+        $opt = key($options->getOpt());
+        $filename = "fen_{$options->getArgs()[0]}_".time().'.csv';
+
+        $sql = "SELECT * FROM games
+            WHERE FEN IS NOT NULL
+            ORDER BY RAND()
+            LIMIT {$options->getArgs()[0]}";
+
+        $games = Pdo::getInstance()
+                    ->query($sql)
+                    ->fetchAll(\PDO::FETCH_ASSOC);
+
+        $dimensions = (new HeuristicPicture(''))->getDimensions();
+
+        $permutations = (new RestrictedPermutationWithRepetition())
+            ->get(
+                [ 5, 8, 13, 21 ],
+                count($dimensions),
+                100
+            );
+
+        $fp = fopen(self::DATA_FOLDER."/$filename", 'w');
+
+        foreach ($games as $game) {
+            try {
+                $sequence = (new Movetext($game['movetext']))->sequence();
+                foreach ($sequence as $movetext) {
+                    $board = (new StringToBoard($game['FEN']))->create();
+                    $heuristicPicture = new HeuristicPicture($movetext, $board);
+                    $balance = $heuristicPicture->take()->getBalance();
+                    $end = end($balance);
+                    $label = (new LinearCombinationLabeller($permutations))->label($end);
+                    $row = array_merge($end, [$label[Symbol::BLACK]]);
+                    fputcsv($fp, $row, ';');
+                }
+            } catch (\Exception $e) {}
+        }
+
+        fclose($fp);
+    }
+}
+
+$cli = new Fen();
+$cli->run();

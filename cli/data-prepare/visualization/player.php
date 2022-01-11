44a3e133d32d731397c3e5ee871630634c21a193
programarivm <info@programarivm.com>
2022-01-11 20:57:44 +0100

    Refactored ChessData\Cli\DataPrepare\Training\Player

--- cli/data-prepare/visualization/player.php
+++ cli/data-prepare/visualization/player.php
@@ -4,31 +4,22 @@ namespace ChessData\Cli\DataPrepare\Training;
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
-use Dotenv\Dotenv;
 use Chess\PGN\Symbol;
-use ChessData\Pdo;
-use splitbrain\phpcli\CLI;
+use ChessData\PdoCli;
 use splitbrain\phpcli\Options;
 
-class Player extends CLI
+class Player extends PdoCli
 {
     const DATA_FOLDER = __DIR__.'/../../../dataset/visualization';
 
-    protected $conf;
-
     protected function setup(Options $options)
     {
-        $dotenv = Dotenv::createImmutable(__DIR__.'/../../../');
-        $dotenv->load();
-
         $options->setHelp('Creates a prepared JSON dataset of heuristics in the dataset/visualization folder.');
         $options->registerArgument('n', 'A random number of games to be queried.', true);
         $options->registerArgument('player', "The chess player's full name.", true);
         $options->registerOption('win', 'The player wins.');
         $options->registerOption('lose', 'The player loses.');
         $options->registerOption('draw', 'Draw.');
-
-        $this->conf = include(__DIR__.'/../../../config/database.php');
     }
 
     protected function main(Options $options)
@@ -49,7 +40,7 @@ class Player extends CLI
             ORDER BY RAND()
             LIMIT {$options->getArgs()[0]}";
 
-        $games = Pdo::getInstance($this->conf)->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
+        $games = $this->pdo->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
 
         $fp = fopen(self::DATA_FOLDER."/$filename", 'w');
         fwrite($fp, json_encode($games));

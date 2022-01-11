81fbf3c103fb90e87a263e100a2c64e49e6315a9
programarivm <info@programarivm.com>
2022-01-11 20:54:41 +0100

    Refactored ChessData\Cli\DataPrepare\Training\Grandmasters

--- cli/data-prepare/grandmasters.php
+++ cli/data-prepare/grandmasters.php
@@ -4,26 +4,17 @@ namespace ChessData\Cli\DataPrepare\Training;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Dotenv\Dotenv;
 use Chess\PGN\Symbol;
-use ChessData\Pdo;
-use splitbrain\phpcli\CLI;
+use ChessData\PdoCli;
 use splitbrain\phpcli\Options;
 
-class Grandmasters extends CLI
+class Grandmasters extends PdoCli
 {
     const DATA_FOLDER = __DIR__.'/../../model';
 
-    protected $conf;
-
     protected function setup(Options $options)
     {
-        $dotenv = Dotenv::createImmutable(__DIR__.'/../../');
-        $dotenv->load();
-
         $options->setHelp('Creates the model/grandmasters.csv file with games by chess grandmasters.');
-
-        $this->conf = include(__DIR__.'/../../config/database.php');
     }
 
     protected function main(Options $options)
@@ -33,7 +24,7 @@ class Grandmasters extends CLI
 
         $sql = "SELECT movetext FROM games WHERE result = '0-1'";
 
-        $games = Pdo::getInstance($this->conf)->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
+        $games = $this->pdo->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
 
         $fp = fopen(self::DATA_FOLDER."/$filename", 'w');
 

bcd3b885daed6b70fe04523de7eb93632e4434d1
programarivm <info@programarivm.com>
2022-01-12 18:23:18 +0100

    Validated movetexts

--- cli/seed/openings.php
+++ cli/seed/openings.php
@@ -4,6 +4,7 @@ namespace ChessData\Cli\Seed;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+use Chess\PGN\Validate;
 use ChessData\PdoCli;
 use splitbrain\phpcli\Options;
 
@@ -33,28 +34,30 @@ class Openings extends PdoCli
         if (is_file($filepath)) {
             $file = fopen($filepath, 'r');
             while (($line = fgetcsv($file)) !== FALSE) {
-                $sql = 'INSERT INTO openings (eco, name, movetext) VALUES (:eco, :name, :movetext)';
-                $values = [
-                    [
-                        'param' => ':eco',
-                        'value' => $line[0],
-                        'type' => \PDO::PARAM_STR,
-                    ],
-                    [
-                        'param' => ':name',
-                        'value' => $line[1],
-                        'type' => \PDO::PARAM_STR,
-                    ],
-                    [
-                        'param' => ':movetext',
-                        'value' => $line[2],
-                        'type' => \PDO::PARAM_STR,
-                    ],
-                ];
+                if ($movetext = Validate::movetext($line[2])) {
+                    $sql = 'INSERT INTO openings (eco, name, movetext) VALUES (:eco, :name, :movetext)';
+                    $values = [
+                        [
+                            'param' => ':eco',
+                            'value' => $line[0],
+                            'type' => \PDO::PARAM_STR,
+                        ],
+                        [
+                            'param' => ':name',
+                            'value' => $line[1],
+                            'type' => \PDO::PARAM_STR,
+                        ],
+                        [
+                            'param' => ':movetext',
+                            'value' => $movetext,
+                            'type' => \PDO::PARAM_STR,
+                        ],
+                    ];
 
-                try {
-                    $this->pdo->query($sql, $values);
-                } catch (\Exception $e) {}
+                    try {
+                        $this->pdo->query($sql, $values);
+                    } catch (\Exception $e) {}
+                }
             }
             fclose($file);
         }

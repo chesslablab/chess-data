2b1b777c4dfba7797f30f2722c03868b9fa8981b
programarivm <info@programarivm.com>
2022-09-06 18:09:06 +0200

    Added autocomplete-players.sql

---
+++ sql/autocomplete-players.sql
@@ -0,0 +1,25 @@
+use chess;
+
+DELETE FROM players WHERE White="Andreikin,Dmitry:Muzychuk,Mariya";
+DELETE FROM players WHERE White="Caruana,Fabiano:Green,Lee";
+DELETE FROM players WHERE White="Giri,Anish:Vujatovic,Rajko";
+DELETE FROM players WHERE White="Ivanishin,Anatoly :Ivan Vagner";
+DELETE FROM players WHERE White="Jones,Gawain C B:Woodward,Clive";
+DELETE FROM players WHERE White="Kramnik,Vladimir:Picot,Russell";
+DELETE FROM players WHERE White="Nakamura,Hikaru:Hodgson,Jeremy";
+DELETE FROM players WHERE White="Short,Nigel D:Baptie,Justin";
+DELETE FROM players WHERE White="Xiangzhi,Bu:Xue Zhao";
+DELETE FROM players WHERE White="Yang,Wen:Yifan,Hou";
+DELETE FROM players WHERE White="Kasparov & Short";
+
+DELETE FROM players WHERE Black="Andreikin,Dmitry:Muzychuk,Mariya";
+DELETE FROM players WHERE Black="Caruana,Fabiano:Green,Lee";
+DELETE FROM players WHERE Black="Giri,Anish:Vujatovic,Rajko";
+DELETE FROM players WHERE Black="Ivanishin,Anatoly :Ivan Vagner";
+DELETE FROM players WHERE Black="Jones,Gawain C B:Woodward,Clive";
+DELETE FROM players WHERE Black="Kramnik,Vladimir:Picot,Russell";
+DELETE FROM players WHERE Black="Nakamura,Hikaru:Hodgson,Jeremy";
+DELETE FROM players WHERE Black="Short,Nigel D:Baptie,Justin";
+DELETE FROM players WHERE Black="Xiangzhi,Bu:Xue Zhao";
+DELETE FROM players WHERE Black="Yang,Wen:Yifan,Hou";
+DELETE FROM players WHERE Black="Kasparov & Short";

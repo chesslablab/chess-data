124fd6d3c30ac26b12ded05835a2e6a448eb8f97
programarivm <info@programarivm.com>
2022-09-07 12:50:09 +0200

    Added ChessData\Cli\DbCleanupCli

---
+++ cli/db-cleanup.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ChessData\Cli;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use ChessData\PdoCli;
+use splitbrain\phpcli\Options;
+
+class DbCleanupCli extends PdoCli
+{
+    const DELETE = [
+        'DELETE FROM players WHERE White="Andreikin,Dmitry:Muzychuk,Mariya" OR Black="Andreikin,Dmitry:Muzychuk,Mariya"',
+        'DELETE FROM players WHERE White="Caruana,Fabiano:Green,Lee" OR Black="Caruana,Fabiano:Green,Lee"',
+        'DELETE FROM players WHERE White="Giri,Anish:Vujatovic,Rajko" OR Black="Giri,Anish:Vujatovic,Rajko"',
+        'DELETE FROM players WHERE White="Ivanishin,Anatoly :Ivan Vagner" OR Black="Ivanishin,Anatoly :Ivan Vagner"',
+        'DELETE FROM players WHERE White="Jones,Gawain C B:Woodward,Clive" OR Black="Jones,Gawain C B:Woodward,Clive"',
+        'DELETE FROM players WHERE White="Kramnik,Vladimir:Picot,Russell" OR Black="Kramnik,Vladimir:Picot,Russell"',
+        'DELETE FROM players WHERE White="Nakamura,Hikaru:Hodgson,Jeremy" OR Black="Nakamura,Hikaru:Hodgson,Jeremy"',
+        'DELETE FROM players WHERE White="Short,Nigel D:Baptie,Justin" OR Black="Short,Nigel D:Baptie,Justin"',
+        'DELETE FROM players WHERE White="Xiangzhi,Bu:Xue Zhao" OR Black="Xiangzhi,Bu:Xue Zhao"',
+        'DELETE FROM players WHERE White="Yang,Wen:Yifan,Hou" OR Black="Yang,Wen:Yifan,Hou"',
+        'DELETE FROM players WHERE White="Kasparov & Short" OR Black="Kasparov & Short"',
+        'DELETE FROM players WHERE White="Carlsen,6" OR Black="Carlsen,6"',
+    ];
+
+    protected function setup(Options $options)
+    {
+        $options->setHelp('Cleans up the chess database.');
+    }
+
+    protected function main(Options $options)
+    {
+        foreach (self::DELETE as $sql) {
+            $this->pdo->query($sql);
+        }
+    }
+}
+
+$cli = new DbCleanupCli();
+$cli->run();

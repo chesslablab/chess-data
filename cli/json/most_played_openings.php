5225dc4aea913e9f1ef9097be59e72966712b96d
programarivm <info@programarivm.com>
2024-11-11 17:10:17 +0100

    Refactored ChessData\Cli\Json\MostPlayedOpenings

--- cli/json/most_played_openings.php
+++ cli/json/most_played_openings.php
@@ -4,11 +4,27 @@ namespace ChessData\Cli\Json;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use ChessData\PdoCli;
+use ChessData\Pdo;
+use Dotenv\Dotenv;
+use splitbrain\phpcli\CLI;
 use splitbrain\phpcli\Options;
 
-class MostPlayedOpenings extends PdoCli
+class MostPlayedOpenings extends CLI
 {
+    protected $pdo;
+
+    public function __construct()
+    {
+        parent::__construct(true);
+
+        $dotenv = Dotenv::createImmutable(__DIR__.'/../../');
+        $dotenv->load();
+
+        $conf = include(__DIR__ . '/../../config/database.php');
+
+        $this->pdo = Pdo::getInstance($conf);
+    }
+
     protected function setup(Options $options)
     {
         $options->setHelp('Creates the output/openings.json file.');

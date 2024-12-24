fc24d0c88f63f480d192c3d69875545ceb1ca9f6
programarivm <info@programarivm.com>
2024-12-24 16:34:43 +0100

    Updated to PHP Chess 1.4.87

--- cli/mine/heuristics.php
+++ cli/mine/heuristics.php
@@ -4,9 +4,9 @@ namespace ChessData\Cli\Mine;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Chess\FenHeuristics;
-use Chess\FenToBoardFactory;
+use Chess\SanSignal;
 use Chess\Function\FastFunction;
+use Chess\Variant\Classical\Board;
 use ChessData\Pdo;
 use Dotenv\Dotenv;
 use splitbrain\phpcli\CLI;
@@ -53,31 +53,29 @@ class Heuristics extends CLI
 
         $rows = $this->pdo->query($sql, $values)->fetchAll(\PDO::FETCH_ASSOC);
 
-        foreach ($rows as $key => $row) {
-            $heuristics = [];
-            foreach (explode(',', $row['fen_mine']) as $val) {
-                $heuristics[] = (new FenHeuristics(
-                    $this->function,
-                    FenToBoardFactory::create($val)
-                ))->balance;
-            }
-
-            $sql = "UPDATE {$this->table} SET heuristics_mine = :heuristics_mine WHERE movetext = :movetext";
-
-            $values = [
-                [
-                    'param' => ':heuristics_mine',
-                    'value' => json_encode($heuristics, true),
-                    'type' => \PDO::PARAM_STR,
-                ],
-                [
-                    'param' => ':movetext',
-                    'value' => $row['movetext'],
-                    'type' => \PDO::PARAM_STR,
-                ],
-            ];
-
+        foreach ($rows as $row) {
             try {
+                $spectrumComponent = (new SanSignal($this->function, $row['movetext'], new Board()))->spectrumComponent;
+
+                array_shift($spectrumComponent);
+
+                $heuristics[] = $spectrumComponent;
+
+                $sql = "UPDATE {$this->table} SET heuristics_mine = :heuristics_mine WHERE movetext = :movetext";
+
+                $values = [
+                    [
+                        'param' => ':heuristics_mine',
+                        'value' => json_encode($heuristics, true),
+                        'type' => \PDO::PARAM_STR,
+                    ],
+                    [
+                        'param' => ':movetext',
+                        'value' => $row['movetext'],
+                        'type' => \PDO::PARAM_STR,
+                    ],
+                ];
+
                 $this->pdo->query($sql, $values);
             } catch (\Exception $e) {
             }
